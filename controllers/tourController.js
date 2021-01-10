bd9e253bef5d9403f564ddf78fc98400e5efc63e
IamSoPrada <9999goldcoin@gmail.com>
2021-01-11 02:36:01 +0300

    Added custom middleware for checking id

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -3,6 +3,17 @@ const fs = require("fs")
 const tours = JSON.parse(
     fs.readFileSync(`${__dirname}/../dev-data/data/tours-simple.json`)
 )
+exports.checkID = (req, res, next, val) => { // мидлвар ф-ция проверяет валидность id до того как достигнет контроллера
+    console.log(`Tour id is : ${val}`)
+    if (req.params.id * 1 > tours.length) {
+        return res.status(404).json({
+            status: "fail",
+            message: 'Invalid ID'
+        })
+    }
+    next()
+}
+
 
 exports.getAllTours = (req, res) => {
     console.log(req.requestTime)
@@ -19,12 +30,6 @@ exports.getAllTours = (req, res) => {
 exports.getTour = (req, res) => {
     console.log(req.params) // здесь дуступны наши переменные которые мы отправляем в запросе :id и  : fye
     const id = req.params.id * 1
-    if (id > tours.length) {
-        return res.status(404).json({
-            status: "fail",
-            message: 'Invalid ID'
-        })
-    }
     const tour = tours.find(el => el.id === id)
     res.status(200).json({
         status: 'Success',
@@ -51,12 +56,7 @@ exports.createTour = (req, res) => {
 }
 
 exports.updateTour = (req, res) => {
-    if (req.params.id * 1 > tours.length) {
-        return res.status(404).json({
-            status: "fail",
-            message: 'Invalid ID'
-        })
-    }
+  
     res.status(200).json({
         status: "success",
         data: {
@@ -66,12 +66,7 @@ exports.updateTour = (req, res) => {
 }
 
 exports.deleteTour = (req, res) => {
-    if (req.params.id * 1 > tours.length) {
-        return res.status(404).json({
-            status: "fail",
-            message: 'Invalid ID'
-        })
-    }
+
     res.status(204).json({
         status: "success",
         data: null
--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -2,6 +2,12 @@ const express = require('express');
 const tourController = require('./../controllers/tourController')
 const router = express.Router() // создали отдельные раутеры для туров и пользователей
 
+//router.param('id', (req, res, next, val) => {
+//    console.log(`Tour id is: ${val}`) // в консоли мы увидим id которое запрашиваем в url
+//    next()
+//}) // когда мы поступает запрос содержащий доп.параметры например id тура, мы можем воспользоваться мидлваром чтобы получить у нему доступ через метод params. Тк это мидлвар нельзя забывать про ф-цию next() иначе req-res цикл застрянет
+
+router.param('id', tourController.checkID)
 
 router.route('/')
     .get(tourController.getAllTours)
