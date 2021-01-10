db502d79f7698b988f4416cc92ac49f2e50d1e99
IamSoPrada <9999goldcoin@gmail.com>
2021-01-10 23:49:42 +0300

    Created crud  f-s for Users routes

--- app.js
+++ app.js
@@ -101,6 +101,37 @@ const deleteTour = (req, res) => {
     })
 }
 
+const getAllUsers = (req, res)=> {
+    res.status(500).json({
+        status: 'error',
+        message: 'This route not yet defined'
+    })
+}
+const getUser = (req, res)=> {
+    res.status(500).json({
+        status: 'error',
+        message: 'This route not yet defined'
+    })
+}
+const createUser = (req, res)=> {
+    res.status(500).json({
+        status: 'error',
+        message: 'This route not yet defined'
+    })
+}
+const updateUser = (req, res)=> {
+    res.status(500).json({
+        status: 'error',
+        message: 'This route not yet defined'
+    })
+}
+const deleteUser = (req, res)=> {
+    res.status(500).json({
+        status: 'error',
+        message: 'This route not yet defined'
+    })
+}
+
 // app.get('/api/v1/tours', getAllTours)
 
 // app.get('/api/v1/tours/:id', getTour)
@@ -121,6 +152,15 @@ app.route('/api/v1/tours/:id')
     .get(getTour)
     .patch(updateTour)
     .delete(deleteTour)
+
+app.route('/api/v1/users')
+    .get(getAllUsers)
+    .post(createUser)
+
+app.route('/api/v1/users/:id')
+    .get(getUser)
+    .patch(updateUser)
+    .delete(deleteUser)
 
 // 4) Start serber
 const port = 3000
