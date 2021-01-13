fd0f2f59ac807d902451c32a7f530a138496e5cf
IamSoPrada <9999goldcoin@gmail.com>
2021-01-13 21:55:59 +0300

    Server part connected to online mongo db with mongoose package; created model schema and Tour schema

--- server.js
+++ server.js
@@ -1,14 +1,48 @@
+const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 const app = require('./app');
 
 dotenv.config({ path: './config.env' });
 
+const DB = process.env.DATABASE.replace(
+  '<PASSWORD>',
+  process.env.DATABASE_PASSWORD
+);
+
+mongoose // подключаем к онлайн бд через монгус(слой асбтр. над монгодб)
+  .connect(DB, {
+    // метод коннект возвращает промис
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false
+  })
+  .then(() => {
+    console.log('DB connection successfull');
+  });
 //console.log(app.get('env'))
 
 //console.log(process.env)
 
-const port = process.env.PORT || 3000
-app.listen(port, () => {
-    console.log(`App running on port ${port}`);
-})
+const tourSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, 'A tour have a name'],
+    unique: true
+  },
+  rating: {
+    type: Number,
+    default: 4.5
+  },
+  price: {
+    type: Number,
+    required: [true, 'A tour must have a price']
+  }
+});
 
+const Tour = mongoose.model('Tour', tourSchema);
+
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`App running on port ${port}`);
+});
