c3809bd4fee366325b4b083dbfd668633a2a2476
IamSoPrada <9999goldcoin@gmail.com>
2021-01-15 21:12:00 +0300

    Added more details to our Tour schema, creted script for importing/deleting data from json to db

---
+++ dev-data/data/import-dev-data.js
@@ -0,0 +1,59 @@
+const fs = require('fs');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+const Tour = require('../../models/tourModel');
+
+dotenv.config({ path: './config.env' });
+
+const DB = process.env.DATABASE.replace(
+  '<PASSWORD>',
+  process.env.DATABASE_PASSWORD
+);
+
+mongoose // подключаем к онлайн бд через монгус(слой асбтр. над монгодб)
+  .connect(DB, {
+    // метод коннект возвращает промис
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false
+  })
+  .then(() => {
+    console.log('DB connection successfull');
+  });
+
+//Читаем файл
+
+const tours = JSON.parse(
+  fs.readFileSync(`${__dirname}/tours-simple.json`, 'utf-8')
+);
+
+// Импортируем данные в бд
+
+const importData = async () => {
+  try {
+    await Tour.create(tours);
+    console.log('Data successfully loaded!');
+  } catch (err) {
+    console.log(err);
+  }
+  process.exit();
+};
+
+// Удаляем все данные из коллекции бд
+
+const deleteData = async () => {
+  try {
+    await Tour.deleteMany();
+    console.log('Data successfully deleted!');
+  } catch (err) {
+    console.log(err);
+  }
+  process.exit();
+};
+
+if (process.argv[2] === '--import') {
+  importData();
+} else if (process.argv[2] === '--delete') {
+  deleteData();
+}
+console.log(process.argv);
