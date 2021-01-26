5984e96687f0e06247348f1fa78fab7009b60eb4
IamSoPrada <9999goldcoin@gmail.com>
2021-01-26 14:50:52 +0300

    Implemented new error handling mechanism in all tour controllers

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -1,6 +1,7 @@
 const Tour = require('./../models/tourModel');
 const APIFeatures = require('./../utils/apiFeatures');
 const catchAsync = require('./../utils/catchAsync');
+const AppError = require('./../utils/appError');
 
 exports.aliasTopTours = (req, res, next) => {
   req.query.limit = '5';
@@ -109,7 +110,7 @@ exports.getAllTours = catchAsync(async (req, res) => {
   });
 });
 
-exports.getTour = catchAsync(async (req, res) => {
+exports.getTour = catchAsync(async (req, res, next) => {
   //console.log(req.params); // здесь дуступны наши переменные которые мы отправляем в запросе :id и  : fye
   //const id = req.params.id * 1;
   /*  const tour = tours.find(el => el.id === id);
@@ -123,6 +124,10 @@ exports.getTour = catchAsync(async (req, res) => {
   const { id } = req.params;
   const tour = await Tour.findById(id); // метод из мангуса
   // Tour.findOne({ _id: req.params.id }); тоже самое
+
+  if (!tour) {
+    return next(new AppError('No tour found with that ID', 404));
+  }
   res.status(200).json({
     status: 'Success',
     data: {
@@ -176,6 +181,9 @@ exports.updateTour = catchAsync(async (req, res, next) => {
     new: true, // доп. условия переписывают пред. данные в бд
     runValidators: true
   });
+  if (!tour) {
+    return next(new AppError('No tour found with that ID', 404));
+  }
   res.status(200).json({
     status: 'success',
     data: {
@@ -185,7 +193,10 @@ exports.updateTour = catchAsync(async (req, res, next) => {
 });
 
 exports.deleteTour = catchAsync(async (req, res, next) => {
-  await Tour.findByIdAndDelete(req.params.id);
+  const tour = await Tour.findByIdAndDelete(req.params.id);
+  if (!tour) {
+    return next(new AppError('No tour found with that ID', 404));
+  }
   res.status(204).json({
     status: 'success',
     data: null
