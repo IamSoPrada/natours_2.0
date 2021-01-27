0599e857f066fd689b2cffdb96ba6a65c2fadda7
IamSoPrada <9999goldcoin@gmail.com>
2021-01-27 22:39:00 +0300

    Created user model

--- app.js
+++ app.js
@@ -26,7 +26,6 @@ app.use(express.static(`${__dirname}/public`)); // 66 урок как смотр
 
 app.use((req, res, next) => {
   req.requestTime = new Date().toISOString();
-  
   next();
   // к поступающему запросу в этом мидлваре мы как бы добавили св-во requestTime
   // которое вызываем в методе getAllTours
